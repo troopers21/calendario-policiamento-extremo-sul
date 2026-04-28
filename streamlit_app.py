a607f385e20ccc5f1a97a72d973392a8c0f4fe13
troopers21 <gabrielsena21@gmail.com>
2026-04-28 14:47:00 -0300

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -60,7 +60,7 @@ st.markdown("""
 col_logo1, col_logo2, col_logo3 = st.columns([2.5, 1.0, 2.5])
 with col_logo2:
     try: 
-        st.image("LogosCircular.png", use_container_width=True)
+        st.image("brasoes_cpr_especializadas.png", use_container_width=True)
     except: 
         pass
 
